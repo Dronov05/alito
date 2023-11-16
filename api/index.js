6006f0ef2754d4723ed7224eb62023e85a015ac5
Max Dronov <23432-az@mail.ru>
2023-11-16 17:13:31 +0300

    Изменил описание того чтв 6 строке

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!!')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// время видео 29-44 установка vim, + еще нужно написать доку по связке гитхаб и сервера
