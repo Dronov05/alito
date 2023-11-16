fb196a636da4394b809c0541aef9d3b7241454a4
Max Dronov <23432-az@mail.ru>
2023-11-16 21:53:30 +0300

    Проверка применённых изменений
    [need deploy server]

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!!')
+    res.send('Я изменён в WebStorm и уже на сервере')
 })
 
 app.listen(port, () => {
