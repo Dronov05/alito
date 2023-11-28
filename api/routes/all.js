0634166520bb684818526d8b0b40c6f58fdc6761
Max Dronov <23432-az@mail.ru>
2023-11-28 12:55:38 +0300

    Исправление
    [need build client]
    [need deploy server]

--- api/routes/all.js
+++ api/routes/all.js
@@ -6,7 +6,8 @@ router.all("*", (req, res, next) => {
 
     const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST
 
-    res.setHeader('Access-Control-Allow-Origin', domain);
+    res.setHeader('Access-Control-Allow-Origin', 'https://allspacex.ru');
+    // res.setHeader('Access-Control-Allow-Origin', domain);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next()
 })
