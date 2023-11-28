a3dedeca3d40bc4f3baf54751039019e1eb535a9
Max Dronov <23432-az@mail.ru>
2023-11-28 12:59:15 +0300

    Исправление
    [need build client]
    [need deploy server]

--- api/routes/all.js
+++ api/routes/all.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-router.all("*", (req, res, next) => {
-
-
-    const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST
-
-    res.setHeader('Access-Control-Allow-Origin', 'https://allspacex.ru');
-    // res.setHeader('Access-Control-Allow-Origin', domain);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next()
-})
+// router.all("*", (req, res, next) => {
+//
+//
+//     const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST
+//
+//     res.setHeader('Access-Control-Allow-Origin', 'https://allspacex.ru');
+//     // res.setHeader('Access-Control-Allow-Origin', domain);
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next()
+// })
 
 module.exports = router
