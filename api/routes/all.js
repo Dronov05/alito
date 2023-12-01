8cdb229d7dbe0b4dd49ad9c26b68e49dd1c50255
Max Dronov <23432-az@mail.ru>
2023-12-01 14:07:49 +0300

    Исправление
    [need build client]
    [need deploy server]

--- api/routes/all.js
+++ api/routes/all.js
@@ -6,8 +6,7 @@ router.all("*", (req, res, next) => {
 
     const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST
 
-    // res.setHeader('Access-Control-Allow-Origin', domain);
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', domain);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Request-Headers', 'content-type');
     next()
